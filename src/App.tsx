da40c25be7332b7167dddc7534d88c08fb0c9689
Etienne TRAN <etiennetran@hotmail.fr>
2023-09-24 15:17:53 +0200

    create switch component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,15 @@ import React from 'react'
 import './App.css'
 import Table from './components/Table'
 import BetInput from './components/BetInput'
+import BetSwitch from './components/BetSwitch'
 
-// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
-function App () {
+function App (): JSX.Element {
   return (
-    <div className="mt-10 flex space-x-10 font-mono">
+    <div className="mt-10 flex items-center space-x-10 font-mono">
         <BetInput id="bet" label="Mise cote boostée"/>
         <BetInput id="quotation-1" label="Cote 1 boostée"/>
         <BetInput id="quotation-2" label="Cote 2"/>
+        <BetSwitch defaultStatus={true} />
         <Table />
     </div>
   )
---
+++ src/components/BetSwitch.tsx
@@ -0,0 +1,48 @@
+import React, { useState } from 'react'
+
+interface BetSwitchProps {
+  defaultStatus?: boolean
+}
+export default function BetSwitch ({ defaultStatus }: BetSwitchProps): JSX.Element {
+  const [isActive, setIsActive] = useState<boolean>(defaultStatus ?? false)
+
+  return (
+    <div className={`
+            relative
+            w-14 h-8
+            px-1
+            flex items-center
+            rounded-full
+            cursor-pointer
+            drop-shadow-lg
+            ${isActive ? 'bg-blue-500' : 'bg-blue-300'}
+        `}
+         onClick={() => { setIsActive(!isActive) }}
+    >
+        <label
+            htmlFor="toggle"
+            className={`
+                absolute
+                w-6 h-6
+                bg-white
+                rounded-full shadow-md
+                flex items-center justify-center
+                transition ease-in-out ${isActive ? 'translate-x-full' : ''}
+            `}
+        >
+            <div className={`
+                    w-3 h-3
+                    rounded-full
+                    ${isActive ? 'bg-blue-500' : 'bg-blue-300'}
+                `}
+            />
+        </label>
+        <input
+            id="toggle"
+            type="checkbox"
+            className="absolute h-0 w-0 overflow-hidden"
+            value={isActive ? 1 : 0}
+        />
+    </div>
+  )
+}
