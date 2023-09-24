a9b3eb605561b3e9fa15892472b19bbc23ea173a
Etienne TRAN <etiennetran@hotmail.fr>
2023-09-24 13:52:00 +0200

    create input component

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import BetInput from './components/BetInput'
 // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
 function App () {
   return (
-    <div className="mt-10 flex space-x-10">
+    <div className="mt-10 flex space-x-10 font-mono">
         <BetInput id="bet" label="Mise cote boostée"/>
         <BetInput id="quotation-1" label="Cote 1 boostée"/>
         <BetInput id="quotation-2" label="Cote 2"/>
--- src/components/BetInput.tsx
+++ src/components/BetInput.tsx
@@ -1,34 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react'
 interface BetInputProps {
   id: string
   label: string
 }
 export default function BetInput ({ id, label }: BetInputProps): JSX.Element {
+  const [inputIsFocused, setInputIsFocused] = useState<boolean>(false)
+  const [textValue, setTextValue] = useState<string>('')
+
+  function inputIsFocusedOrHasTextValue (): boolean {
+    return inputIsFocused || textValue !== ''
+  }
+
   return (
     <div className="
-        relative
-        flex items-center justify-center
-        h-10 w-32
-        rounded-md
-        bg-blue-200
-        focus:outline-non"
+          relative z-1
+          flex items-center justify-center
+          h-10
+          rounded-md"
     >
       <label
           id={`${id}-label`}
-          className="absolute"
+          className={`
+            absolute z-20
+            p-2
+            transition ease-in-out
+            ${inputIsFocusedOrHasTextValue() ? '-translate-y-5 bg-white text-xs text-blue-500' : 'text-base'}
+          `}
           htmlFor={label}
       >
           {label}
       </label>
       <input
           id={id}
-          className="
-            relative z-10
+          className={`
+            relative
+            ${inputIsFocusedOrHasTextValue() ? 'z-1' : 'z-20'}
+            px-1
             w-full h-full
-            rounded-md outline-blue-500
-            bg-transparent"
+            rounded-md border border-blue-300 outline-blue-500
+            bg-transparent
+            text-base text-center
+            caret-blue-500
+            `}
           type="text"
           name={label}
+          onFocus={() => { setInputIsFocused(true) }}
+          onBlur={() => { setInputIsFocused(false) }}
+          onChange={(event) => { setTextValue(event.target.value) }}
       />
     </div>
   )
