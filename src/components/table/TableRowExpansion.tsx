bb810ec1a43e591197bdab753b38ef2e7bc5a9f3
Etienne TRAN <etiennetran@hotmail.fr>
2024-05-10 10:58:22 +0200

    CC-24 : Ajouter une transition sur le expansion des lignes du tableau

--- src/components/table/TableRowExpansion.tsx
+++ src/components/table/TableRowExpansion.tsx
@@ -1,20 +1,40 @@
-import React, { type ReactElement } from 'react'
+import React, { useState, useEffect, type ReactElement } from 'react'
+import clsx from 'clsx'
 import { type ReactElementProps } from '../../interfaces/ReactElementPropsInterface'
 
 interface TableRowExpansionProps extends ReactElementProps {
   isExpanded: boolean
+  close?: () => void
 }
 
-export default function TableRowExpansion ({ isExpanded, children }: TableRowExpansionProps): ReactElement {
-  if (!isExpanded) {
-    return (<></>)
-  }
+export default function TableRowExpansion ({ isExpanded, children, close }: TableRowExpansionProps): ReactElement {
+  const [showChildren, setShowChildren] = useState(false)
+
+  useEffect(() => {
+    let timeoutId: NodeJS.Timeout
+
+    if (isExpanded) {
+      timeoutId = setTimeout(() => { setShowChildren(true) }, 200)
+      return
+    }
+
+    setShowChildren(false)
+
+    return () => { clearTimeout(timeoutId) }
+  }, [isExpanded])
 
   return (
-        <tr className="border border-violet-100">
-            <td colSpan={6}>
-                { children }
-            </td>
-        </tr>
+    <tr
+      className={clsx(
+        'border border-violet-100',
+        'transition-all ease-in-out duration-300',
+        isExpanded ? 'h-20' : 'h-0')
+      }
+      onClick={close}
+    >
+      <td colSpan={6} className={clsx(!showChildren && 'hidden')}>
+        { children }
+      </td>
+    </tr>
   )
 }
