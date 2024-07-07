82d9061f233593b405f46052c0662007f9bc23cf
Etienne Tran <62669050+Mysto91@users.noreply.github.com>
2024-07-07 11:53:11 +0200

    CC-68 : [Bug] La valeur de la probabilité doit être tronquée (#69)

--- src/tests/unit/truncate.test.ts
+++ src/tests/unit/truncate.test.ts
@@ -3,3 +3,11 @@ import { truncate } from '../../utils/truncate'
 it('test with float having 3 decimals returns float with 2 decimals', () => {
   expect(truncate(2.556)).toEqual(2.56)
 })
+
+it('test with float having 4 decimals and asking for 3 decimals returns float with 3 decimals', () => {
+  expect(truncate(3.5556, 3)).toEqual(3.556)
+})
+
+it('test with float having multiple decimals returns float with 2 decimals', () => {
+  expect(truncate(57.999999)).toEqual(58)
+})
